011ebadb9d8122271fad99d8e538d4adadad5ec6
cometscome <cometscome@gmail.com>
2021-10-21 13:28:23 +0900

    binary

--- src/PIMDtools.jl
+++ src/PIMDtools.jl
@@ -614,7 +614,7 @@ module PIMDtools
         seekposition = 0
         count = 0
         seekposition += nbytes_Int64
-        for itrj=istart:itrj-1
+        for i=istart:itrj-1
             count += 1
             seekposition += 2*nbytes_Int64
             for iatom=1:trjfile.numatoms_list[count]
